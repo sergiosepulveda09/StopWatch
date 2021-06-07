6b998514ff0ab4d7497c05f93f7a5192cfea235c
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-06-06 19:19:04 -0700

    Add files via upload

---
+++ StopWatch/StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by Sergio Sepulveda on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
